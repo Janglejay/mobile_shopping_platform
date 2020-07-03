236fc1ddd2ed713c37a5d248ed25f64543e7b80f
ForU <18644045901@163.com>
2020-07-03 17:22:10 +0800

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mobile_shopping_platform
+
+基于Flutter开发的移动购物平台
+
---
+++ android/app/src/main/kotlin/com/example/mobile_shopping_platform/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_shopping_platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
